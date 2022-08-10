999e44428a1036e5f9351cbaa9a378131468a718
seanbelknap <sean.belknap@gmail.com>
2022-08-10 07:10:27 -0600

    added useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./App.css";
 import GroceryList from "./listAndTables";
 import WelcomeBack from "./WelcomeBack";
@@ -5,31 +6,41 @@ import WelcomeBack from "./WelcomeBack";
 import Quote from "./Quote";
 
 function EncouragingWords(props) {
-  console.log(props)
+  console.log(props);
   return <p>Believe you can and you’re halfway there.</p>;
-
 }
 
 function DailyTip() {
   return <p>Highlight a whole paragraph by triple-clicking on it.</p>;
 }
 
-
 const quote = {
-  text:
-    "I am great believer in luck, and I find the harder I work, the more I have of it.",
+  text: "I am great believer in luck, and I find the harder I work, the more I have of it.",
   author: "Thomas Jefferson",
 };
 
+const handleSubscribe = (e) => console.log("User has been subscribed...");
+
 function App() {
+  const [subscribed, setSubscribed] = useState(false);
+  const [alerts, setAlerts] = useState(false);
+
+  console.log("Subscribed Status:", subscribed);
   return (
     <>
       <WelcomeBack name="Joe" />
       <Quote quote={quote} />
+      <section>
+        <p>Please click to subscribe to my updates!</p>
+        <button onClick={() => setSubscribed(!subscribed)}>
+          {subscribed ? "Unsubscribe" : "Subscribe"}
+        </button>
+        <button onClick={() => setAlerts(!alerts)}>
+        {alerts ? "Stop Email Alerts" : "Get Email Alerts"}
+      </button>
+      </section>
     </>
   );
 }
 
-
-
-export default App; 
+export default App;
